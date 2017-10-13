6cfc3371181d07058d9c73aba8465750a1e03ee6
Thom Allen <thomwork@gmail.com>
2017-10-13 08:30:19 +0100

    Implement pug view templates & routes for index page

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const path = require('path');
+// import routes
+const index = require('./routes/index');
+// g0
 const app = express();
-
+// cfg
 const PORT = 3333;
 
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
+// view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// routes
+app.use('/', index);
+
+// app.get('/', function(req, res) {
+//   res.send('hello world');
+// });
 
 app.listen(PORT, function() {
   console.log('listening on port', PORT);
