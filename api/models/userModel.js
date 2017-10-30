142ec1fd1c27c434b9cb7844c2cfa22f69f54cc9
Thom Allen <thomwork@gmail.com>
2017-10-30 12:24:00 +0000

    Update userModel

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -2,7 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  name: {
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cPassword: {
     type: String,
     required: true,
   }
