266236f1f2a864e19a8dd107067bc27479236f1b
Thom Allen <thomwork@gmail.com>
2017-10-30 15:57:11 +0000

    Add sample middleware for login-required pages

--- api/routes/profileRoutes.js
+++ api/routes/profileRoutes.js
@@ -3,7 +3,16 @@ var router = express.Router();
 require('../models/userModel.js');
 var cont = require('../controllers/userController.js');
 
-router.get('/', function (req, res, next) {
+function loginRequired(req, res, next) {
+  if (req.session && req.session.username) {
+    return next();
+  } else {
+    res.send('log in to view page')
+    return next();
+  }
+}
+
+router.get('/', loginRequired, function (req, res, next) {
   var username = req.session.username;
   req.flash('info', 'flasherino!', JSON.stringify(req.session, null, 2))
   res.render('profile', {
