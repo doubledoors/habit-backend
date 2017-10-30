bcca942e97caf883856189f4ac21315159fb3bb5
Thom Allen <thomwork@gmail.com>
2017-10-30 13:01:42 +0000

    Scaffold basic user registration & login

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+require('../models/userModel.js');
+var cont = require('../controllers/userController.js');
 // render index view
 router.get('/', function (req, res, next) {
   req.flash('info', 'flash!')
   res.render('index', { title: 'habit-backend', flash: req.flash('info') });
 });
+
+router.post('/register', cont.CREATE_USER);
+router.post('/login', cont.LOGIN_USER);
 
 module.exports = router;
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -5,9 +5,6 @@ var cont = require('../controllers/userController.js');
 
 router.get('/', cont.GET_USERS);
 
-router.post('/create', cont.CREATE_USER);
-
 router.post('/:userId/delete', cont.DELETE_USER);
-
 
 module.exports = router;
